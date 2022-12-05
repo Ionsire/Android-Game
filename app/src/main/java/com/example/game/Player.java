1b23adc09bccedbe8a6e9fa041b2c02e79389916
Inamar <inamar@pop-os.localdomain>
2022-12-05 18:01:17 -0300

    Create player animation

--- app/src/main/java/com/example/game/Player.java
+++ app/src/main/java/com/example/game/Player.java
@@ -7,6 +7,8 @@ import android.graphics.BitmapFactory;
 import static com.example.game.GameView.screenRatioX;
 import static com.example.game.GameView.screenRatioY;
 
+import java.util.ArrayList;
+
 public class Player {
 
     int frameIndex = 0;
@@ -22,11 +24,15 @@ public class Player {
     // Imagens da animacao
     Bitmap sprite1, sprite2, sprite3;
 
+    //ArrayList<Bitmap> spriteArray = new ArrayList<Bitmap>();
+
     Player(Resources res){
         width = (int) (width * screenRatioX);
         height = (int) (height * screenRatioY);
 
         sprite1 = BitmapFactory.decodeResource(res, R.drawable.player);
+        sprite2 = BitmapFactory.decodeResource(res, R.drawable.player2);
+        sprite3 = BitmapFactory.decodeResource(res, R.drawable.player3);
 
         width = sprite1.getWidth();
         height = sprite1.getHeight();
@@ -40,6 +46,12 @@ public class Player {
         height = (int) (height * screenRatioY);
 
         sprite1 = Bitmap.createScaledBitmap(sprite1, width, height,false);
+        sprite2 = Bitmap.createScaledBitmap(sprite2, width, height,false);
+        sprite3 = Bitmap.createScaledBitmap(sprite3, width, height,false);
+//        spriteArray.add(sprite1);
+//        spriteArray.add(sprite2);
+//        spriteArray.add(sprite3);
+
 
     }
     Bitmap walkAnimation(){
@@ -47,9 +59,15 @@ public class Player {
             frameIndex++;
             return sprite1;
         }
-        frameIndex--;
+        else if(frameIndex == 1){
+            frameIndex++;
+            return sprite2;
+        }
 
-        return sprite2;
+        // Reset sprite index
+        frameIndex=0;
+        return sprite3;
+
     }
 
 }
